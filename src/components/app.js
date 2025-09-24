6b7065ee30ed00c45de8655e609f05a89b66a60e
Aruize <anruiesc@gmail.com>
2025-09-24 21:35:33 +0200

    created recent-post and searchbar component & added home

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 
 import Logo from './logo';
+import SearchBar from './searchbar';
+import RecentPosts from './recent-posts';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Logo />
-
-        <div class="search">
-          <input type="text" placeholder="daily smarty" />
-        </div>
-
-        <div class="recent">
-          <ul>
-            <li>post</li>
-            <li>post</li>
-            <li>post</li>
-          </ul>
-        </div>
+        <SearchBar />
+        <RecentPosts />
 
       </div>
 
