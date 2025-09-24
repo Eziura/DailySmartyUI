7ef3a5ec7311a6b749a020b177e22bec5ea23a7c
Aruize <anruiesc@gmail.com>
2025-09-24 23:22:53 +0200

    axios to get portfolioitems

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,20 @@
+import { SET_RECENT_POSTS } from './types.js';
+import axios from 'axios';
+
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        console.log('fetching recent posts');
+        axios.get(
+            'https://eziura.devcamp.space/portfolio/portfolio_items')
+            .then(response => {
+                console.log('response', response.data.portfolio_items);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.portfolio_items
+                });
+            }) 
+            .catch(error => {
+                console.log('error', error);
+            });
     }
 }
