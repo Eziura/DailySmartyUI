479866d1bf5050059a14ae92b3f7741dd2008700
Aruize <anruiesc@gmail.com>
2025-09-25 12:25:54 +0200

    setup redux devtools & reducer set_recent_posts fix

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import {thunk} from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 import './style/main.scss';
 
@@ -16,7 +20,7 @@ import Results from './components/results';
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}> 
       <BrowserRouter>
         <Switch>
           <Route path='/' exact component={Home} />
