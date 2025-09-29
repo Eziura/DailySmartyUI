b5942461e8f48913a0956e96a802ae9fc051c18a
Aruize <anruiesc@gmail.com>
2025-09-29 16:42:51 +0200

    create results-post component

--- src/components/results.js
+++ src/components/results.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import Logo from './logo';
 import SearchBar from './searchbar';
+import ResultsPosts from './results-posts';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class Results extends Component {
     handleSearchBarSubmit(query) {
         this.props.fetchResultsWithQuery(query);
-        
+
     }
     render() {
         return (
             <div>
                 <Logo size={50} />
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+                < ResultsPosts />
             </div>
         )
     }
